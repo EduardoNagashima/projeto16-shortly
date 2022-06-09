844ba402a97217dea4ee3130e85534f7db8df860
Eduardo <eduyudji@gmail.com>
2022-06-09 16:52:42 -0300

    endpoints url done

--- back-end/src/controllers/urlController.js
+++ back-end/src/controllers/urlController.js
@@ -5,17 +5,63 @@ export async function postUrl(req, res){
     const {user} = res.locals;
     const {url} = req.body;
     const nanoid = customAlphabet('1234567890abcdef', 8);
-    const shortUrl = nanoid(url);
-    console.log(user.rows[0].id)
+    const shortUrl = nanoid();
+    console.log(user.rows[0].id, shortUrl);
     try{
         await db.query(`
-        INSERT INTO "usersLinks"("fullLink","shortLink", "userId") VALUES ($1, $2, $3);
+        INSERT INTO "usersLinks" ("fullLink","shortLink", "userId") VALUES ($1, $2, $3);
         `,[url, shortUrl, user.rows[0].id]);
         const obj = {
-            shortUrl
+            "shortUrl": shortUrl
         }
-        res.status(201).send(obj);
+        res.status(201).send('ae');
     } catch (e){
+        console.log(e);
+        return res.status(500).send('Não foi possível se conectar com o BD')
+    }
+}
+
+export async function getUrlId(req, res){
+    const {id} = req.params;
+
+    try{
+        const url = await db.query(`
+            SELECT * FROM "usersLinks" 
+            WHERE id = $1
+        `,[id]);
+
+        if (url.rowCount === 0) return res.sendStatus(404);
+
+        const obj = {
+            "id": id,
+            "shortUrl": url.rows[0].shortLink,
+            "url": url.rows[0].fullLink
+        }
+        return res.status(200).send(obj);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send('Não foi possível se conectar com o BD')
+    }
+}
+
+export async function convertUrl(req, res){
+    const {shortUrl} = req.params;
+    try{
+        const linkInfo = await db.query(`
+        SELECT * FROM "usersLinks"
+        WHERE "shortLink" = $1
+        `,[shortUrl]);
+
+        if (linkInfo.rowCount === 0) return res.sendStatus(404);
+        
+        await db.query(`
+            UPDATE "usersLinks"
+            SET "views"= $1
+        `,[(parseInt(linkInfo.rows[0].views) + 1)]);
+
+        //ENTENDER COMO O REDIRECT FUNCIONA
+        return res.redirect(linkInfo.rows[0].fullLink);
+    } catch (e) {
         console.log(e);
         return res.status(500).send('Não foi possível se conectar com o BD')
     }
--- back-end/src/routers/urlRouter.js
+++ back-end/src/routers/urlRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 import {authMiddleware} from "./../middlewares/authMiddleware.js";
-import {postUrl} from "./../controllers/urlController.js";
+import {postUrl, getUrlId, convertUrl} from "./../controllers/urlController.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", authMiddleware, postUrl);
-// urlRouter.get("/urls/:id", getUrlId);
-// urlRouter.get("/urls/open/:shortUrl", getShortUrl);
+urlRouter.get("/urls/:id", getUrlId);
+urlRouter.get("/urls/open/:shortUrl", convertUrl);
 // urlRouter.delete("/urls/shorten", authMiddleware, deleteUrl);
 
 export default urlRouter;
