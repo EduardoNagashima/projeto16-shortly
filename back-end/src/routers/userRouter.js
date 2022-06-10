f23e5cda6bb4222775cda9d9d4a1b667de81ef29
Eduardo <eduyudji@gmail.com>
2022-06-10 15:14:10 -0300

    bugfix userRouter auth

--- back-end/src/routers/userRouter.js
+++ back-end/src/routers/userRouter.js
@@ -6,6 +6,6 @@ import {authMiddleware} from "./../middlewares/authMiddleware.js";
 const userRouter = Router();
 
 userRouter.get('/users/:id', authMiddleware, getUsers);
-userRouter.get('/ranking', authMiddleware, getRanking);
+userRouter.get('/ranking', getRanking);
 
 export default userRouter;
